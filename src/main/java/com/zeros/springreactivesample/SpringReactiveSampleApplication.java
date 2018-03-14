fa1a7f27ba974018e9436da509a0104380ebedb7
zeroual <abid1991@hotmail.fr>
2018-03-14 14:55:44 +0100

    generate the app form start.spring.io

---
+++ src/main/java/com/zeros/springreactivesample/SpringReactiveSampleApplication.java
@@ -0,0 +1,12 @@
+package com.zeros.springreactivesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveSampleApplication.class, args);
+	}
+}
